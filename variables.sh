1256002c2a283a87f50fb44486896a7ff32d150c
manoj515876 <manojputtaluckey@gmail.com>
2025-09-01 14:31:26 +0530

    Taking Input username and password

--- variables.sh
+++ variables.sh
@@ -8,6 +8,10 @@ echo "Hello $name"
 echo "Hi $1"
 echo "Hi $2, How can I help you"
 
+echo "Enter your username"
 read USERNAME
 echo "Your username is $USERNAME"
+
+echo "Enter your password"
+read -s PASSWD
 
