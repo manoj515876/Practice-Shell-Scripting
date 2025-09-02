31bac5a0a0ed83bda5056a6743ff942f0b98743e
manoj515876 <manojputtaluckey@gmail.com>
2025-09-02 15:10:00 +0530

    Logs Practice

--- variables.sh
+++ variables.sh
@@ -148,7 +148,7 @@
 
 # Logs 
 
-LOG_FOLDER="/var/log/shellscripts.logs"
+LOG_FOLDER="/var/log/shellscripts-logs"
 LOG_FILE=$(echo $0 | cut -d "." -f1)
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE_NAME="$LOG_FOLDER/$LOG_FILE-$TIMESTAMP.log"
