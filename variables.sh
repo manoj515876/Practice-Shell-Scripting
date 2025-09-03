deb42185820558f1e9b2cc9dc1edec22029405a8
manoj515876 <manojputtaluckey@gmail.com>
2025-09-03 16:26:47 +0530

    Packages installing script

--- variables.sh
+++ variables.sh
@@ -209,7 +209,57 @@
 
 # Loops 
 
-for i in {1..10}
+# for i in {1..10}
+# do 
+#     echo $i
+# done 
+
+
+# Using loops installing the packages 
+
+USERID=$(id -u)
+N="\e[30m"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+
+LOG_FOLDER="/var/log/shellscript-logs"
+LOG_FILE=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME="$LOG_FOLDER/$LOG_FILE-$TIMESTAMP.log"
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then 
+        echo -e "$2 ... $R FAILURE $N"
+        exit 1 
+    else 
+        echo -e "$2 ... $G SUCCESS $N"
+    fi 
+}
+
+CHECK_ROOT(){
+    if [ $USERID -ne 0 ]
+    then 
+        echo "ERROR:: You must need sudo access to execute this script"
+        exit 1 
+    fi 
+}
+
+mkdir -p $LOG_FOLDER
+
+echo "Script started executing at: $TIMESTAMP" &>>$LOG_FILE_NAME
+
+CHECK_ROOT
+
+for package in $@ 
 do 
-    echo $i
+    dnf list installed package &>>$LOG_FILE_NAME
+    if [ $? -ne 0 ]
+    then 
+        dnf install $package -y &>>$LOG_FILE_NAME
+        VALIDATE $? "Installing $package"
+    else 
+        echo -e "$package is already $Y ... INSTALLED $N"
+    fi
 done 
