4d993878bc373ac35f194d66ca5c6b8197f0aaaf
manoj515876 <manojputtaluckey@gmail.com>
2025-09-03 16:34:11 +0530

    Packages installing script

--- variables.sh
+++ variables.sh
@@ -217,49 +217,51 @@
 
 # Using loops installing the packages 
 
+
+
 USERID=$(id -u)
-N="\e[30m"
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
+N="\e[0m"
 
-LOG_FOLDER="/var/log/shellscript-logs"
-LOG_FILE=$(echo $0 | cut -d "." -f1)
+LOGS_FOLDER="/var/log/shellscript-logs"
+LOG_FILE=$(echo $0 | cut -d "." -f1 )
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
-LOG_FILE_NAME="$LOG_FOLDER/$LOG_FILE-$TIMESTAMP.log"
+LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
 
 VALIDATE(){
     if [ $1 -ne 0 ]
-    then 
+    then
         echo -e "$2 ... $R FAILURE $N"
-        exit 1 
-    else 
+        exit 1
+    else
         echo -e "$2 ... $G SUCCESS $N"
-    fi 
+    fi
 }
+
+mkdir -p $LOGS_FOLDER
 
 CHECK_ROOT(){
     if [ $USERID -ne 0 ]
-    then 
-        echo "ERROR:: You must need sudo access to execute this script"
-        exit 1 
-    fi 
+    then
+        echo "ERROR:: You must have sudo access to execute this script"
+        exit 1 #other than 0
+    fi
 }
-
-mkdir -p $LOG_FOLDER
 
 echo "Script started executing at: $TIMESTAMP" &>>$LOG_FILE_NAME
 
 CHECK_ROOT
 
-for package in $@ 
-do 
-    dnf list installed package &>>$LOG_FILE_NAME
+for package in $@
+do
+    dnf list installed $package &>>$LOG_FILE_NAME
     if [ $? -ne 0 ]
-    then 
+    then
         dnf install $package -y &>>$LOG_FILE_NAME
         VALIDATE $? "Installing $package"
-    else 
-        echo -e "$package is already ... INSTALLED $N"
+    else
+        echo -e "$package is already $Y ... INSTALLED $N"
     fi
-done 
+done
